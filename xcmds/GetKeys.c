21e2c12011bc46311191c86c74265a09fcbd74e4
Brian Kendall <brian@briankendall.net>
2023-05-21 00:47:04 -0400

    Convert XCMD source files to unix line endings

--- xcmds/GetKeys.c
+++ xcmds/GetKeys.c
@@ -1,1 +1,117 @@
-/* XFCN that will determine if keys are pressed given keycodes Takes one parameter: a string of up to eight comma separated integers, each representing a key code Returns: a string of comma separated boolean values, each corresponding to the key code in the passed in string, where true indicates the key is down  Example: The user is pressing Shift+A  GetKeys("56,0,12")  ...returns: "true,true,false"*/#include "HyperXCmd.h"#define kMaxKeyCodeCount 8Boolean keyIsDown(short keyCode, KeyMap *keyMap){	short byteIndex;	char theByte, theBit;	char *thePointer;		if (keyCode < 0) {		return false;	}		byteIndex = keyCode >> 3;	thePointer = (char *)(*keyMap);	theByte = thePointer[byteIndex];	theBit = 1L << (keyCode & 7);	return ((theByte & theBit) != 0);}pascal void main(XCmdPtr paramPtr){	KeyMap keyMap;	short keyCodes[kMaxKeyCodeCount];	long temp;	int keyCodeCount, i;	char *input;	unsigned char numStr[5];	unsigned char w;	int inputLength;	Handle handle;		if (paramPtr->paramCount != 1 || paramPtr->params[0] == nil) {		return;	}		w = 0;	keyCodeCount = 0;	input = *paramPtr->params[0];	inputLength = StringLength(paramPtr, input);		for(i = 0; i <= inputLength; ++i) {		if (w >= 4) {			return;		}				if (i != inputLength && input[i] != ',') {			numStr[w+1] = input[i];			++w;			continue;		}				if (w > 0) {			numStr[0] = w;			temp = StrToNum(paramPtr, numStr);						if (temp < 0 || temp >= 128) {				return;			}						keyCodes[keyCodeCount] = (short)temp;		} else {			keyCodes[keyCodeCount] = -1;		}				w = 0;		++keyCodeCount;				if (keyCodeCount >= kMaxKeyCodeCount) {			break;		}	}		GetKeys(keyMap);	w = 0;	handle = NewHandle(keyCodeCount*6);		for(i = 0; i < keyCodeCount; ++i) {		Boolean down = keyIsDown(keyCodes[i], &keyMap);				if (i != 0) {			(*handle)[w++] = ',';		}				if (down) {			(*handle)[w++] = 't';			(*handle)[w++] = 'r';			(*handle)[w++] = 'u';			(*handle)[w++] = 'e';		} else {			(*handle)[w++] = 'f';			(*handle)[w++] = 'a';			(*handle)[w++] = 'l';			(*handle)[w++] = 's';			(*handle)[w++] = 'e';		}	}		(*handle)[w] = '\0';	paramPtr->returnValue = handle;}
+/*
+ XFCN that will determine if keys are pressed given keycodes
+ Takes one parameter: a string of up to eight comma separated integers, each
+ representing a key code
+ Returns: a string of comma separated boolean values, each corresponding to the
+ key code in the passed in string, where true indicates the key is down
+ 
+ Example:
+ The user is pressing Shift+A
+ 
+ GetKeys("56,0,12")
+ 
+ ...returns: "true,true,false"
+*/
+
+#include "HyperXCmd.h"
+
+#define kMaxKeyCodeCount 8
+
+Boolean keyIsDown(short keyCode, KeyMap *keyMap)
+{
+	short byteIndex;
+	char theByte, theBit;
+	char *thePointer;
+	
+	if (keyCode < 0) {
+		return false;
+	}
+	
+	byteIndex = keyCode >> 3;
+	thePointer = (char *)(*keyMap);
+	theByte = thePointer[byteIndex];
+	theBit = 1L << (keyCode & 7);
+	return ((theByte & theBit) != 0);
+}
+
+pascal void main(XCmdPtr paramPtr)
+{
+	KeyMap keyMap;
+	short keyCodes[kMaxKeyCodeCount];
+	long temp;
+	int keyCodeCount, i;
+	char *input;
+	unsigned char numStr[5];
+	unsigned char w;
+	int inputLength;
+	Handle handle;
+	
+	if (paramPtr->paramCount != 1 || paramPtr->params[0] == nil) {
+		return;
+	}
+	
+	w = 0;
+	keyCodeCount = 0;
+	input = *paramPtr->params[0];
+	inputLength = StringLength(paramPtr, input);
+	
+	for(i = 0; i <= inputLength; ++i) {
+		if (w >= 4) {
+			return;
+		}
+		
+		if (i != inputLength && input[i] != ',') {
+			numStr[w+1] = input[i];
+			++w;
+			continue;
+		}
+		
+		if (w > 0) {
+			numStr[0] = w;
+			temp = StrToNum(paramPtr, numStr);
+			
+			if (temp < 0 || temp >= 128) {
+				return;
+			}
+			
+			keyCodes[keyCodeCount] = (short)temp;
+		} else {
+			keyCodes[keyCodeCount] = -1;
+		}
+		
+		w = 0;
+		++keyCodeCount;
+		
+		if (keyCodeCount >= kMaxKeyCodeCount) {
+			break;
+		}
+	}
+	
+	GetKeys(keyMap);
+	w = 0;
+	handle = NewHandle(keyCodeCount*6);
+	
+	for(i = 0; i < keyCodeCount; ++i) {
+		Boolean down = keyIsDown(keyCodes[i], &keyMap);
+		
+		if (i != 0) {
+			(*handle)[w++] = ',';
+		}
+		
+		if (down) {
+			(*handle)[w++] = 't';
+			(*handle)[w++] = 'r';
+			(*handle)[w++] = 'u';
+			(*handle)[w++] = 'e';
+		} else {
+			(*handle)[w++] = 'f';
+			(*handle)[w++] = 'a';
+			(*handle)[w++] = 'l';
+			(*handle)[w++] = 's';
+			(*handle)[w++] = 'e';
+		}
+	}
+	
+	(*handle)[w] = '\0';
+	paramPtr->returnValue = handle;
+}
--- xcmds/ScreenBounds.c
+++ xcmds/ScreenBounds.c
@@ -1,1 +1,53 @@
-/* XFCN that returns the width and height of the screen in pixels*/#include "HyperXCmd.h"struct QDGlobals {	char privates[76];	long randSeed;	BitMap screenBits;	Cursor arrow;	Pattern dkGray;	Pattern ltGray;	Pattern gray;	Pattern black;	Pattern white;	GrafPtr thePort;};typedef struct QDGlobals QDGlobals;typedef QDGlobals *QDGlobalsPtr;pascal void main(XCmdPtr paramPtr){	Str255 output;	Str255 heightStr;	short width, height;	short widthLength, heightLength;	short i;	QDGlobalsPtr qdGlobals;		// How to get access to the QuickDraw globals from an XCMD / XFCN:	// Apparently the value of SetCurrentA5() will be the address of	// thePort, and we can use that to find the rest of the QuickDraw	// global variables	qdGlobals = (QDGlobalsPtr)(*((Handle)SetCurrentA5()) - sizeof(QDGlobals) + sizeof(thePort));		width = qdGlobals->screenBits.bounds.right - qdGlobals->screenBits.bounds.left;	height = qdGlobals->screenBits.bounds.bottom - qdGlobals->screenBits.bounds.top;	NumToStr(paramPtr, width, output);	NumToStr(paramPtr, height, heightStr);		widthLength = output[0];	heightLength = heightStr[0];	output[widthLength+1] = ',';		for(i = 0; i < heightLength; ++i) {		output[widthLength+2+i] = heightStr[i+1];	}		output[0] = widthLength+heightLength+1;		paramPtr->returnValue = PasToZero(paramPtr, output);}
+/*
+ XFCN that returns the width and height of the screen in pixels
+*/
+
+#include "HyperXCmd.h"
+
+struct QDGlobals {
+	char privates[76];
+	long randSeed;
+	BitMap screenBits;
+	Cursor arrow;
+	Pattern dkGray;
+	Pattern ltGray;
+	Pattern gray;
+	Pattern black;
+	Pattern white;
+	GrafPtr thePort;
+};
+typedef struct QDGlobals QDGlobals;
+typedef QDGlobals *QDGlobalsPtr;
+
+pascal void main(XCmdPtr paramPtr)
+{
+	Str255 output;
+	Str255 heightStr;
+	short width, height;
+	short widthLength, heightLength;
+	short i;
+	QDGlobalsPtr qdGlobals;
+	
+	// How to get access to the QuickDraw globals from an XCMD / XFCN:
+	// Apparently the value of SetCurrentA5() will be the address of
+	// thePort, and we can use that to find the rest of the QuickDraw
+	// global variables
+	qdGlobals = (QDGlobalsPtr)(*((Handle)SetCurrentA5()) - sizeof(QDGlobals) + sizeof(thePort));
+	
+	width = qdGlobals->screenBits.bounds.right - qdGlobals->screenBits.bounds.left;
+	height = qdGlobals->screenBits.bounds.bottom - qdGlobals->screenBits.bounds.top;
+	NumToStr(paramPtr, width, output);
+	NumToStr(paramPtr, height, heightStr);
+	
+	widthLength = output[0];
+	heightLength = heightStr[0];
+	output[widthLength+1] = ',';
+	
+	for(i = 0; i < heightLength; ++i) {
+		output[widthLength+2+i] = heightStr[i+1];
+	}
+	
+	output[0] = widthLength+heightLength+1;
+	
+	paramPtr->returnValue = PasToZero(paramPtr, output);
+}
